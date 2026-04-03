5a6dc1031f0d6b4e5a842c0e0cec5c1fe0915544
Alvaro <alvarohj96sg@gmail.com>
2026-04-03 14:35:16 +0200

    feat: Fixed niños and styles

--- src/app/services/rsvp.service.ts
+++ src/app/services/rsvp.service.ts
@@ -3,12 +3,12 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export interface RsvpData {
-  nombre:        string;
-  asistencia:    string;
-  personas:      number;
-  ninos:         number;
+  nombre: string;
+  asistencia: string;
+  personas: number;
+  ninos: number;
   restricciones: string[];
-  mensaje:       string;
+  mensaje: string;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -16,15 +16,16 @@ export class RsvpService {
 
   private readonly SHEET_URL = 'https://script.google.com/macros/s/AKfycbz7LNYKoeiiWmCzUqS3GNQLLlG36AODfEhhRB7YC5EES7z3UaQoSsxUAsu46VQcelA1/exec';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   enviarRsvp(data: RsvpData): Observable<any> {
     const params = new HttpParams()
-      .set('nombre',        data.nombre)
-      .set('asistencia',    data.asistencia)
-      .set('personas',      String(data.personas))
+      .set('nombre', data.nombre)
+      .set('asistencia', data.asistencia)
+      .set('personas', String(data.personas))
+      .set('ninos', String(data.ninos))
       .set('restricciones', data.restricciones.join(', ') || 'Ninguna')
-      .set('mensaje',       data.mensaje);
+      .set('mensaje', data.mensaje);
 
     return this.http.post(this.SHEET_URL, params.toString(), {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
